718323bb07fc926be0399dcf32f07c30d781174b
ACER <baxtiyor.gazimboyev@gmail.com>
2024-10-15 14:23:21 +0300

    js

---
+++ add.js
@@ -0,0 +1,110 @@
+// // 1-misol
+// let sm=prompt("uzunligini kiritin sm")
+// sm = parseFloat(sm)
+// let m =parseFloat(sm /100)
+// alert(`siz kiritgan uzunlig ${m} metrga teng`)
+
+
+
+// 2-misol
+// let kg=prompt("soni  kiritin kg")
+// kg = parseFloat(kg)
+// let t =parseFloat(kg /1000)
+// alert(`siz kiritgan og'irligi ${t} to'naga teng`)
+
+
+
+// 3-misol
+//  let kb=prompt("soni  kiritin kb")
+//  kb = parseFloat(kb)
+//  let t =parseFloat(kb *1240)
+//  alert(`siz kiritgan og'irligi ${t} baytga  teng`)
+
+
+
+
+// 4-misol
+// let a=prompt("a  kiritin ")
+// let b=prompt("b  kiritin ")
+// let c =parseFloat(a/b)
+// alert(c)
+
+
+
+// 5-misol
+//  let a=prompt("a  kiritin ")
+//  let b=prompt("b  kiritin ")
+//  let c =a%b
+//  alert(c)
+
+
+
+
+// 6-misol
+// let son=+prompt("ikkihonali soni kiritin");
+// let onliklar = Math.floor(son / 10);
+
+// let birxonali = son % 10;
+// alert(`o'nliklar xonasidagi son ${onliklar}, bir honali son ${birxonali}`);
+
+// 7-misol
+
+// let son=+prompt("ikkihonali soni kiritin");
+// let birxonali = son % 10;
+// let onliklar = Math.floor(son / 10);
+// let yigindi = (onliklar + birxonali);
+// alert(`${yigindi}`);
+
+
+
+// 8-misol
+// let a = prompt("ikki honali son kiritin");
+// let c=parseInt(a/10%10);
+// let d =a%10;
+// alert(`${d}${c}`)
+
+
+
+// 9-misol
+// let a = prompt("3 honali son kiritin");
+// let b=parseInt(a/100);
+// let c=parseInt(a/10%10);
+// let d =a%10;
+// alert(`${b}`);
+
+
+// 10-misol
+// let a = prompt("3 honali son kiritin");
+// let b=parseInt(a/100);
+// let c=parseInt(a/10%10);
+// let d =a%10;
+// alert(`${d}${c}`);
+
+
+
+// 11-misol
+// let a=+prompt("3 soni kiritin");
+// let yuzliklar=parseInt(a/100)
+// let birxonali = a % 10;
+// let onliklar = parseInt(a/10%10);
+// let yigindi = (onliklar + birxonali + yuzliklar );
+// alert(`${yigindi}`);
+
+
+//12-misol
+// let a = prompt("3 honali son kiritin");
+// let b=parseInt(a/100)
+// let c=parseInt(a/10%10);
+// let d =a%10;
+// alert(`${d}${c}${b}`)
+
+
+// 13-misol
+let a = prompt("3 honali son kiritin");
+let b=parseInt(a/100)
+let c=parseInt(a/10%10);
+let d =a%10;
+alert(`${c}${d}${b}`)
+
+
+// qo'shimcha
